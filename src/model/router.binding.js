f7c876f46a66a77e7779b3ea030c6172fd1c3ac0
thoughtsunificator <oss@thoughtsunificator.me>
2021-12-05 21:56:33 +0000

    removeEventListener

--- src/model/router.binding.js
+++ src/model/router.binding.js
@@ -10,6 +10,7 @@ import RouterEventListener from "./router.event.js"
  */
 class RouterBinding extends Binding {
 
+
 	/**
 	 * @param {object} properties
 	 * @param {Router} properties.router
@@ -18,22 +19,30 @@ class RouterBinding extends Binding {
 		super(properties, new RouterEventListener(properties.router))
 	}
 
+	onPopState(event) {
+		const { router } = this.properties
+		if(router.type === Router.TYPE.HASH) {
+			let path = this.root.ownerDocument.location.hash.slice(1)
+			if(path === "") {
+				router.emit("navigate", new Link("/"))
+			} else {
+				router.emit("browse", new Link(path))
+			}
+		} else if(router.type === Router.TYPE.PATHNAME) {
+			router.emit("browse", new Link(this.root.ownerDocument.location.pathname))
+		}
+	}
+
+	remove() {
+		this.root.ownerDocument.defaultView.removeEventListener("popstate", event => this.onPopState(event))
+		super.remove()
+	}
+
 	onCreated() {
 
 		const { router } = this.properties
 
-		this.root.ownerDocument.defaultView.addEventListener("popstate", () => {
-			if(router.type === Router.TYPE.HASH) {
-				let path = this.root.ownerDocument.location.hash.slice(1)
-				if(path === "") {
-					router.emit("navigate", new Link("/"))
-				} else {
-					router.emit("browse", new Link(path))
-				}
-			} else if(router.type === Router.TYPE.PATHNAME) {
-				router.emit("browse", new Link(this.root.ownerDocument.location.pathname))
-			}
-		})
+		this.root.ownerDocument.defaultView.addEventListener("popstate", event => this.onPopState(event))
 
 		if(router.initialPath !== null) {
 			if(router.type === Router.TYPE.VIRTUAL) {
